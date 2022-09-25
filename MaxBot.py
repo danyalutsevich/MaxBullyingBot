0ca850024df5be8e2c0a55c89f03dd28c21fc71b
danyalutsevich <luchevich31@gmail.com>
2022-09-25 20:22:47 +0300

    pong

--- MaxBot.py
+++ MaxBot.py
@@ -8,7 +8,7 @@ from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.bot import Bot
 
-updater = Updater(token='5504760756:AAFiG1aZUlYtxmMGVvVgJuikPG1OVI9-J-M', use_context=True)
+updater = Updater(token='5441776897:AAHIsMl17B1avefQ97kYuQWzWzEldfL8_Ak', use_context=True)
 
 bullying_messages = ["МАКС СДЕЛАЙ ДЗ"]
 
@@ -22,7 +22,7 @@ def ping(update: Update, context: CallbackContext):
     update.message.reply_text('pong')
 
 
-dispatcher1.add_handler(MessageHandler(Filters.text, echo))
 dispatcher1.add_handler(CommandHandler('ping', ping))
+dispatcher1.add_handler(MessageHandler(Filters.text, echo))
 
 updater.start_polling()
